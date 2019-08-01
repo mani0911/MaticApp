02468512793edcdfdf8e55aec846d9da7f8fa9c6
Manish kumar <manish0992kumar@gmail.com>
2019-08-01 13:53:46 +0530

    Add landing view controller

--- MaticApp/AppDelegate.swift
+++ MaticApp/AppDelegate.swift
@@ -10,12 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let navigationController = UINavigationController(rootViewController: LandingViewController())
+        navigationController.navigationBar.isHidden = true
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
